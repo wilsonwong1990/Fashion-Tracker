8d17b604764e65d6f209f71f485f1da58155fb1f
Wilson Wong <wilsonwong1990@gmail.com>
2022-11-06 20:14:36 -0800

    adding an update helper

---
+++ GH-action-update.py
@@ -0,0 +1,10 @@
+"""
+A helper to be called by GitHub Actions to run an update against csvs.
+
+Author: Wilson Wong
+Date: 11-06-2022
+"""
+
+import yoox
+
+yoox.update_yoox_csv("store-csvs/yoox.csv")
